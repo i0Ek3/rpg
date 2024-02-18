f75686c69eb47c040370e8461b05e52e4c3d166c
i0Ek3 <kno30826@gmail.com>
2024-02-18 11:55:45 +0800

    style: 'black' the code style

--- app.py
+++ app.py
@@ -4,27 +4,37 @@ import string
 
 app = Flask(__name__)
 
+
 def genpwd(length):
-    characters = string.ascii_letters + string.digits + '!@#$%&*-'
-    password = random.choice(string.ascii_letters + string.digits) # 确保密码不以特殊字符开头
-    password += ''.join(random.choice(characters) for _ in range(length - 2))
-    password += random.choice(string.ascii_letters + string.digits) # 确保密码不以特殊字符结尾
+    characters = string.ascii_letters + string.digits + "!@#$%&*-"
+    password = random.choice(
+        string.ascii_letters + string.digits
+    )  # 确保密码不以特殊字符开头
+    password += "".join(random.choice(characters) for _ in range(length - 2))
+    password += random.choice(
+        string.ascii_letters + string.digits
+    )  # 确保密码不以特殊字符结尾
     return password
 
+
 def genpwd2():
-    password = ''
+    password = ""
     for _ in range(3):
-        segment = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(4))
-        password += segment + '-'
+        segment = "".join(
+            random.choice(string.ascii_letters + string.digits) for _ in range(4)
+        )
+        password += segment + "-"
     password = password[:-1]  # 移除末尾的连接符-
     return password
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        pwda, pwdb = genpwd2(), genpwd(random.randint(8, 12))
-        return render_template('index.html', password=pwda, password2=pwdb)
-    return render_template('index.html')
 
-if __name__ == '__main__':
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        pwda, pwdb = genpwd2(), genpwd(random.randint(8, 12))
+        return render_template("index.html", password=pwda, password2=pwdb)
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
     app.run()
